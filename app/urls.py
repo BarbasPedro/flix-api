d131e95d3e6282ef05a2b05d5de19d5a4c15414f
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-07-21 15:00:00 -0300

    feat: add genres url and views

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.http import JsonResponse
 from django.urls import path
+from genres.views import genre_create_list_view, genre_detail_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("genres/", genre_create_list_view, name='genres'),
+    path("genres/<int:pk>/", genre_detail_view, name='genres-detail')
 ]
